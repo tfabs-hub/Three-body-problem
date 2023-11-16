00dfc1c0f2762baf00bf2715931cc69d18caebd7
tfabs-hub <64419030+tfabs-hub@users.noreply.github.com>
2023-11-16 17:53:08 -0500

    Fixed! almost there

--- Lagrange Points.py
+++ Lagrange Points.py
@@ -2,34 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 G = 1  
-m1 = m2 = 1  
-Omega = 1 
+m1 = 2
+m2 = 1
+Omega = 1
 
 #meshgrid
-x = np.linspace(-1.5, 1.5, 400)
-y = np.linspace(-1.5, 1.5, 400)
+x = np.linspace(-3, 3, 400)
+y = np.linspace(-3, 3, 400)
 X, Y = np.meshgrid(x, y)
 
-#Defining Radii
-r1 = np.sqrt((X + 0.5)**2 + Y**2)
-r2 = np.sqrt((X - 0.5)**2 + Y**2)
+#effective potential
+r1 = np.sqrt((X+1)**2 + Y**2)
+r2 = np.sqrt((X-1)**2 + Y**2)
+#Ueff = -G*m1/np.abs(np.sqrt(X**2 + Y**2)-r1) - G*m2/np.abs(np.sqrt(X**2 + Y**2)-r2) - 0.5*Omega**2*(X**2 + Y**2)
+Ueff = -G * (m1 / r1 + m2 / r2) + 0.5 * Omega**2 * (X**2 + Y**2)
 
-# Effective potential -- NOT SURE ABOUT THIS STILL
-Ueff = -G*m1/r1 - G*m2/r2 - 0.5*Omega**2*(X**2 + Y**2)
+Ueff_min = np.min(Ueff)
+Ueff_max = np.max(Ueff)
 
 
+
+fig, ax = plt.subplots()
+
+
+fig.patch.set_facecolor('black')
 plt.figure(figsize=(6, 6))
-plt.imshow(Ueff, extent=(x.min(), x.max(), y.min(), y.max()), origin='lower', cmap='RdGy', alpha=0.5)
-plt.colorbar(label='Effective potential')
-
-
-plt.plot(-0.5, 0, 'bo')
-plt.plot(0.5, 0, 'ro')
-
-
-plt.title('Effective potential and Lagrange points')
+plt.contour(X, Y, Ueff, levels=np.linspace(Ueff_min, Ueff_max, 100), cmap='RdYlBu')
+plt.title('Effective Potential')
 plt.xlabel('x')
 plt.ylabel('y')
+plt.colorbar(label='Ueff')
 plt.grid(True)
 plt.show()
--- untitled1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 16 15:15:40 2023
-
-@author: tommyfabian
-"""
-
