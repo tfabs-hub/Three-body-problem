cc9b1fb61fe1b15f0022b6933037d619b8f619dc
tfabs-hub <64419030+tfabs-hub@users.noreply.github.com>
2023-12-14 14:14:25 -0500

    fafa

--- Lagrange point tests.py
+++ Lagrange point tests.py
@@ -9,23 +9,42 @@ from nbody import solve_n_body_problem, Body, plot_helper
 
 
 ##L1 Lagrange Points
-'''bodies = [Body(50, np.array([-1.0,0.0,0.0]), np.array([0.0,3.8,0.0])),
+bodies = [Body(50, np.array([-1.0,0.0,0.0]), np.array([0.0,3.8,0.0])),
           Body(50, np.array([1.0,0.0,0.0]), np.array([0.0,-3.8,0.0])),
-          Body(2, np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0]))]'''
+          Body(2, np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0]))]
 
+flat_r_arr = solve_n_body_problem(bodies, 0.0001, 50000)
 
+plot_helper(bodies, flat_r_arr)
+plt.title('L1 in Rotating Frame')
+plt.show()
 
 ##L2/L3 Lagrange Points
-'''bodies = [Body(2, np.array([0.0,-0.5,0.0]), np.array([-1.0,0,0.0])),
+bodies = [Body(2, np.array([0.0,-0.5,0.0]), np.array([-1.0,0,0.0])),
           Body(2, np.array([0.0,0.5,0.0]), np.array([1.0,0.0,0.0])),
-          Body(.01, np.array([0.0,-.5992,0.0]), np.array([1.1984,0.0,0.0]))]'''
-          
+          Body(.01, np.array([0.0,-.5992,0.0]), np.array([1.1984,0.0,0.0]))]
+         
+
+flat_r_arr = solve_n_body_problem(bodies, 0.0001, 50000)
+
+plot_helper(bodies, flat_r_arr)
+plt.title('L2/L3 in Rotating Frame')
+plt.show()
    
 ##L4/5 Lagrange Points
-'''bodies = [Body(25, np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0])),
+bodies = [Body(25, np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0])),
           Body(1, np.array([1.0,0.0,0.0]), np.array([0.0,5.099,0.0])),
           Body(.01, np.array([-0.4615,0.866,0.0]), np.array([-4.33,-2.36,0.0]))
-          ]'''
+          ]
+
+
+
+flat_r_arr = solve_n_body_problem(bodies, 0.0001, 50000)
+
+plot_helper(bodies, flat_r_arr)
+plt.title('L4/L5 in Rotating Frame')
+plt.show()
+
 
 ## Figure eight
 
